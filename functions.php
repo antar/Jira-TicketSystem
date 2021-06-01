f1636884deae2397a63d097f10a4e16059bdf34d
antar <toobrokeforrysll@gmail.com>
2021-06-01 08:51:09 +0200

    feat: created footer and header functions

--- functions.php
+++ functions.php
@@ -13,5 +13,48 @@ function pdo_connect_mysql() {
     }
     return $pdo;
 }
-
+// Template footer
+function template_footer() {
+echo <<<EOT
+    </body>
+</html>
+EOT;
+}
+// Template admin header
+function template_admin_header($title, $selected = 'dashboard') {
+    $admin_links = '
+        <a href="index.php"' . ($selected == 'dashboard' ? ' class="selected"' : '') . '><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+        <a href="tickets.php"' . ($selected == 'tickets' ? ' class="selected"' : '') . '><i class="fas fa-ticket-alt"></i>Tickets</a>
+        <a href="comments.php"' . ($selected == 'comments' ? ' class="selected"' : '') . '><i class="fas fa-comments"></i>Comments</a>
+        <a href="accounts.php"' . ($selected == 'accounts' ? ' class="selected"' : '') . '><i class="fas fa-users"></i>Accounts</a>
+        <a href="categories.php"' . ($selected == 'categories' ? ' class="selected"' : '') . '><i class="fas fa-list"></i>Categories</a>
+        <a href="emailtemplate.php"' . ($selected == 'emailtemplate' ? ' class="selected"' : '') . '><i class="fas fa-envelope"></i>Email Templates</a>
+        <a href="settings.php"' . ($selected == 'settings' ? ' class="selected"' : '') . '><i class="fas fa-tools"></i>Settings</a>
+    ';
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,minimum-scale=1">
+        <title>$title</title>
+        <link href="admin.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    </head>
+    <body class="admin">
+        <aside class="responsive-width-100 responsive-hidden">
+            <h1>Admin Panel</h1>
+            $admin_links
+        </aside>
+        <main class="responsive-width-100">
+            <header>
+                <a class="responsive-toggle" href="#">
+                    <i class="fas fa-bars"></i>
+                </a>
+                <div class="space-between"></div>
+                <a href="about.php" class="right"><i class="fas fa-question-circle"></i></a>
+                <a href="logout.php" class="right"><i class="fas fa-sign-out-alt"></i></a>
+            </header>
+EOT;
+}
 ?>
