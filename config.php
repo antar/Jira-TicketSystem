1cebacdcb1e147e2a55036d0164c20386a214132
antar <toobrokeforrysll@gmail.com>
2021-06-01 11:11:57 +0200

    feat: new definitions

--- config.php
+++ config.php
@@ -1,12 +1,22 @@
 <?php
 // database hostname, you don't usually need to change this
-define('db_host', '*');
+define('db_host', 'localhost');
 // database username
-define('db_user', '*');
+define('db_user', 'root');
 // database password
 define('db_pass', '*');
 // database name
-define('db_name', '*');
+define('db_name', 'm120');
 // database charset, change this only if utf8 is not supported by your language
-define('db_charset', '*');
+define('db_charset', 'utf8');
+// Change "Your Company Name" and "yourdomain.com", do not remove the < and >
+define('mail_from', 'M120 Jira <admin@wes.fm>');
+// View ticket link, this will be used for the ticket emails
+define('view_ticket_link', 'https://jira.wes.fm/ticketsystem/view.php');
+// Authentication required?
+define('authentication_required', true);
+// Number of tickets to show per page
+define('num_tickets_per_page', 5);
+// Maximum allowed upload image file size
+define('max_allowed_upload_image_size', 500000);
 ?>
