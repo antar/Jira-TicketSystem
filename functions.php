803c86c75e80b181e101255d3a4ddb1982d50d28
antar <toobrokeforrysll@gmail.com>
2021-06-01 08:58:44 +0200

    fix: template correct now

--- functions.php
+++ functions.php
@@ -5,13 +5,40 @@ include_once 'config.php';
 function pdo_connect_mysql() {
     try {
         // Connect to the MySQL database using PDO...
-    	$pdo = new PDO('mysql:host=' . db_host . ';dbname=' . db_name . ';charset=' . db_charset, db_user, db_pass);
+        $pdo = new PDO('mysql:host=' . db_host . ';dbname=' . db_name . ';charset=' . db_charset, db_user, db_pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $exception) {
-    	// Could not connect to the MySQL database, if this error occurs make sure you check your db settings are correct!
-    	exit('Failed to connect to database!');
+        // Could not connect to the MySQL database, if this error occurs make sure you check your db settings are correct!
+        exit('Failed to connect to database!');
     }
     return $pdo;
+}
+
+// Template header, feel free to customize this
+function template_header($title) {
+$login_link = isset($_SESSION['account_loggedin']) ? '<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>' : '<a href="login.php"><i class="fas fa-lock"></i>Login</a>';
+$admin_link = isset($_SESSION['account_loggedin']) && $_SESSION['account_role'] == 'Admin' ? '<a href="admin/index.php" target="_blank"><i class="fas fa-cog"></i>Admin</a>' : '';
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,minimum-scale=1">
+        <title>$title</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    </head>
+    <body>
+    <nav class="navtop">
+        <div>
+            <h1><a href="index.php">M120 Jira</a></h1>
+            <a href="create.php"><i class="fas fa-plus"></i>Create Ticket</a>
+            <a href="tickets.php"><i class="fas fa-ticket-alt"></i>Tickets</a>
+            $admin_link
+            $login_link
+        </div>
+    </nav>
+EOT;
 }
 // Template footer
 function template_footer() {
@@ -55,6 +82,30 @@ echo <<<EOT
                 <a href="about.php" class="right"><i class="fas fa-question-circle"></i></a>
                 <a href="logout.php" class="right"><i class="fas fa-sign-out-alt"></i></a>
             </header>
+EOT;
+}
+// Template admin footer
+function template_admin_footer() {
+echo <<<EOT
+        </main>
+        <script>
+        document.querySelector(".responsive-toggle").onclick = function(event) {
+            event.preventDefault();
+            let aside = document.querySelector("aside"), main = document.querySelector("main"), header = document.querySelector("header");
+            let asideStyle = window.getComputedStyle(aside);
+            if (asideStyle.display == "none") {
+                aside.classList.remove("closed", "responsive-hidden");
+                main.classList.remove("full");
+                header.classList.remove("full");
+            } else {
+                aside.classList.add("closed", "responsive-hidden");
+                main.classList.add("full");
+                header.classList.add("full");
+            }
+        };
+        </script>
+    </body>
+</html>
 EOT;
 }
 ?>
