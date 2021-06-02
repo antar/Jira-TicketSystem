8566856514ff9afb169e4f80f2e5933181aa35dd
antar <toobrokeforrysll@gmail.com>
2021-06-02 10:14:24 +0200

    addded about.php

---
+++ admin/about.php
@@ -0,0 +1,16 @@
+<?php
+include 'main.php';
+?>
+<?=template_admin_header('About', 'about')?>
+
+<h2>About</h2>
+
+<div class="content-block">
+
+    <p>The ticketing system developed by <a href="https://github.com/antar" target="_blank">Alessio Carcavallo</a>.</p>
+
+    <p>You are currently using <b>Version 1.1.0</b>.</p>
+
+</div>
+
+<?=template_admin_footer()?>
