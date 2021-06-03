544eafdab1307f9cfb0858a1fbc07570155aaac3
antar <toobrokeforrysll@gmail.com>
2021-06-03 08:56:34 +0200

    feat: for future purpose emailtemplate

---
+++ admin/emailtemplate.php
@@ -0,0 +1,14 @@
+<?php
+include 'main.php';
+?>
+<?=template_admin_header('Email Template', 'emailtemplate')?>
+
+<h2>Email Template</h2>
+
+<div class="content-block">
+    <td>
+    Hello there, this option is currently disabled because of SMTP Problems with the Server.
+    </td>
+</div>
+
+<?=template_admin_footer()?>
